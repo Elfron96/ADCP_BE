46cf319cef8ab9978bde477e2e7edc20627d6e77
Elfron96 <b.lecoeuvre@gmail.com>
2022-01-24 15:52:49 +0100

    Q3 ok

--- BE_ADCP.py
+++ BE_ADCP.py
@@ -1,9 +1,11 @@
+from traceback import print_tb
 import numpy as np
 import scipy.io as sci
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 from scipy.linalg import lstsq
+import matplotlib.dates as mdates
 
 
 # 1)
@@ -44,7 +46,7 @@ MES_mg_l = data_calib[:,1]
 
 M = NTU_calib[:, np.newaxis]**[0, 1]
 p, res, rnk, s = lstsq(M, MES_mg_l)
-print(p)
+# print(p)
 y = p[0] + p[1]*NTU_calib
 plt.figure()
 plt.scatter(NTU_calib, MES_mg_l)
@@ -75,4 +77,120 @@ plt.xlabel('Date (YYYY:MM:DD)')
 plt.ylabel('Turbidité de fond')
 plt.legend()
 
+# plt.show()
+
+
+# 2 
+awac = sci.loadmat('awac.mat')
+awac_data = pd.DataFrame(awac['awac'][0][0][0].astype(int), columns = ['year','month','day','hour','minute','second'])
+awac_data['date']=pd.to_datetime(awac_data)
+
+awac_data['pressure'] = awac['awac'][0][0][5].astype(float)
+# awac_data['a1'] = awac['awac'][0][0][1].astype(float)
+# awac_data['a2'] = awac['awac'][0][0][2].astype(float)
+# awac_data['a3'] = awac['awac'][0][0][3].astype(float)
+
+
+mask_tps_data = awac_data[(awac_data['date'] < pd.Timestamp(2018,12,30)) & (awac_data['date'] > pd.Timestamp(2018,12,15))].index
+
+# print(mask_tps_data)
+awac_data['pressure_masked'] = awac_data.iloc[mask_tps_data][['pressure']]
+awac_data['date_masked'] = awac_data.iloc[mask_tps_data][['date']]
+# print(awac_data['pressure_masked'])
+
+plt.figure()
+plt.plot(awac_data['date_masked'], awac_data['pressure_masked'], label='Hauteur d\'eau')
+plt.title('Hauteur d\'eau')
+plt.xlabel('Date (YYYY:MM:DD)')
+plt.ylabel('Hauteur, en m')
+plt.legend()
+
+
+table_hauteur = np.ones((40,1))
+table_hauteur*=0.5
+table_hauteur[1]=1.4
+for i in range(38):
+    table_hauteur[2+i] = table_hauteur[1+i] + 0.5 
+table_hauteur[0]=0.9
+# table_hauteur += 0.3
+
+a1 = awac['awac'][0][0][1].astype(float)[mask_tps_data[0]:mask_tps_data[-1]+1,:]
+a2 = awac['awac'][0][0][2].astype(float)[mask_tps_data[0]:mask_tps_data[-1]+1,:]
+a3 = awac['awac'][0][0][3].astype(float)[mask_tps_data[0]:mask_tps_data[-1]+1,:]
+
+ve = awac['awac'][0][0][6].astype(float)[mask_tps_data[0]:mask_tps_data[-1]+1,:]
+vn = awac['awac'][0][0][7].astype(float)[mask_tps_data[0]:mask_tps_data[-1]+1,:]
+
+for i in range(len(awac_data['date_masked'][mask_tps_data[0]:mask_tps_data[-1]+1])):
+    for j in range(len(table_hauteur)-1):
+        if awac_data['pressure'][i]<=table_hauteur[j]:
+            a1[i,j]=np.NaN
+            a2[i,j]=np.NaN
+            a3[i,j]=np.NaN
+            ve[i,j]=np.NaN
+            vn[i,j]=np.NaN
+
+# print(ve.shape)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(211)
+ax.set_xlabel('Temps')
+plt.imshow(ve.T,origin="lower",extent=[mdates.date2num(pd.Timestamp(2018,12,15)),mdates.date2num(pd.Timestamp(2018,12,30)),0,40],aspect='auto',cmap='RdBu')
+plt.colorbar()
+ax.xaxis_date()
+date_format = mdates.DateFormatter('%D:%H:%M')
+ax.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+plt.title('Vitesse EST')
+
+ax = fig.add_subplot(212)
+ax.set_xlabel('Temps')
+plt.imshow(vn.T,origin="lower",extent=[mdates.date2num(pd.Timestamp(2018,12,15)),mdates.date2num(pd.Timestamp(2018,12,30)),0,40],aspect='auto',cmap='RdBu')
+plt.colorbar()
+ax.xaxis_date()
+date_format = mdates.DateFormatter('%D:%H:%M')
+ax.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+plt.title('Vitesse NORD')
+
 plt.show()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(221)
+ax.set_xlabel('Temps')
+plt.imshow(a1.T,origin="lower",extent=[mdates.date2num(pd.Timestamp(2018,12,15)),mdates.date2num(pd.Timestamp(2018,12,30)),0,40],aspect='auto',cmap='rainbow')
+ax.xaxis_date()
+date_format = mdates.DateFormatter('%D:%H:%M')
+ax.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+plt.colorbar()
+plt.title('Backscatter A1')
+
+ax1 = fig.add_subplot(222)
+ax1.set_xlabel('Temps')
+plt.imshow(a2.T,origin="lower",extent=[mdates.date2num(pd.Timestamp(2018,12,15)),mdates.date2num(pd.Timestamp(2018,12,30)),0,40],aspect='auto',cmap='rainbow')
+ax1.xaxis_date()
+date_format = mdates.DateFormatter('%D:%H:%M')
+ax1.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+plt.colorbar()
+plt.title('Backscatter A2')
+
+ax = fig.add_subplot(223)
+ax.set_xlabel('Temps')
+plt.imshow(a3.T,origin="lower",extent=[mdates.date2num(pd.Timestamp(2018,12,15)),mdates.date2num(pd.Timestamp(2018,12,30)),0,40],aspect='auto',cmap='rainbow')
+ax.xaxis_date()
+date_format = mdates.DateFormatter('%D:%H:%M')
+ax.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+plt.colorbar()
+plt.title('Backscatter A3')
+
+plt.show()
+
+
+# print(awac_data.head())
+# awac['date']=pd.to_datetime(awac)
+# mask_air_data = awac[(awac['date'] < pd.Timestamp(2018,12,30))].index
